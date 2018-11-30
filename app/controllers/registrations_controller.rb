3dab5a7d606e6df7149320f0bc2849666f19c79a
maxharris1 <drumaxter@gmail.com>
2018-11-29 20:22:08 -0600

    added user auth

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,21 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit( :fist_Name,
+                                  :last_Name,
+                                  :email,
+                                  :password,
+                                  :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit( :first_Name,
+                                  :last_Name,
+                                  :email,
+                                  :password,
+                                  :password_confirmation,
+                                  :current_password)
+  end
+end
